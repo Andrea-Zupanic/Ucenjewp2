f1d622abbec452d5bee4580abe87914602ff7ef8
Andrea Županić <andrea.zupanic10@gmail.com>
2023-12-07 14:36:43 +0100

    update

--- SQLedunova.sql
+++ SQLedunova.sql
@@ -128,9 +128,10 @@ insert into clanovi (grupa,polaznik) values
 
 select * from clanovi;
 
---UPDATE
 
 
+
+                                                                     --UPDATE
 
 
 update polaznici set prezime='Kartik' where sifra=17;
@@ -138,6 +139,7 @@ update polaznici set prezime='Kartik' where sifra=17;
 update polaznici set prezime='Šuler',
 oib='96868832693', brojugovora='25/20023'
 where sifra=17;
+
 
 
 select * from polaznici where sifra=17;
@@ -164,32 +166,48 @@ update smjerovi set cijena=1489.52 where sifra=2;
 
 select* from smjerovi;
 
+update mjesta set postanskiBroj=31431 where sifra=1945;
 
 
---INSERT
+-- mijenjao sam samo jednu kolonu (prezime)
+update polaznici set prezime='Kartik' 
+where sifra=17;
+
+-- mjenjam tri kolone (prezime, oib, brojugovora)
+update polaznici set prezime='�uler',
+oib='80352353849' , brojugovora='25/20023'
+where sifra=17;
+
+
+                                                            --INSERT
 
 
 
 insert smjerovi (naziv)
 values ('python programiranje');
+
+                                                --SELECT COUNT, DISTINCT
+
+select count(*) from kupci;
+
+-- izvucite sve kupce koji se zovu kao i Vi.
+
+-- izvucite jedinstvena imena i prezimena kupaca
+-- koji se zovu kao i Vi
+
+select distinct ime, prezime
+from kupci where ime='Tomislav';
+
+
+                                                              --DELETE
+
 delete from  smjerovi where sifra=4;
 
-
-
-
---DELETE
-
-
 --delete from clanovi where polaznik=20;
-select * from polaznici;
-
---edunova
-select * from grupe;
-
-select * from smjerovi where sifra=1;
 
 
---INNER JOIN  
+
+                                                            --INNER JOIN  
 
 
 
@@ -210,9 +228,10 @@ on c.sifra=a.predavac
 inner join clanovi d on a.sifra=d.grupa
 inner join polaznici e on d.polaznik=e.sifra;
 ;
+
+
+
 --knjiznica
-
-
 use knjiznica;
 
 select * from katalog;
@@ -229,16 +248,15 @@ where a.naslov like '%ljubav%'
 --svastara
 use svastara;
 
+
+
+
+
+                                                     --COUNT 
 select count(*) from kupci;
 
 select distinct ime, prezime from
 kupci where ime='andrea';
-
-
-
-
---COUNT 
-
 
 select count(*) 
 from artikli 
@@ -250,33 +268,30 @@ on a.sifra=b.opcina
 where a.naziv = 'Čepin';
 
 
-update mjesta set postanskiBroj=31431 where sifra=1945;
-
 
 select b.ime, b.prezime
 from mjesta a inner join kupci b
 on b.mjesto= a.sifra
 where a.naziv='Livana';
 
-
---COUNT
-
-
 select b.ime, b.prezime
 from mjesta a inner join kupci b
 on b.mjesto= a.sifra
 where a.naziv='Livana';
+
 
 
 select count(*) from primke where datum between '2017-01-01' and '2017-12-31';
 
+                                                                      
+																	  --TOP 1
 
-select top 1* from primke where datum between '2017-01-01' and '2017-12-31';
+
+select top 1* from primke where datum
+between '2017-01-01' and '2017-12-31';
 
 
-
---CIJENA NA PRIMCI , ZBRAJANJE, order by
-
+                                                 --CIJENA NA PRIMCI , ZBRAJANJE, order by
 
 
 select c.kratkinaziv, b.kolicina, b.cijena, b.kolicina * b.cijena as iznos
@@ -287,7 +302,7 @@ where a.rednibroj = '14778/2017'
 ORDER BY 4 desc;
 
 
---UKUPNI IZNOS SUM
+                                                           --UKUPNI IZNOS SUM
 
 
 select sum (b.kolicina * b.cijena) as ukupniiznos
@@ -298,7 +313,7 @@ where a.rednibroj = '14778/2017';
 
 
 
---IZNOSI NA SVEMU , poredaj 
+                                                       --IZNOSI NA SVEMU , poredaj 
 
 select a.rednibroj,  sum (b.kolicina * b.cijena) as ukupniiznos
 from primke a 
@@ -308,7 +323,7 @@ where a.datum between '2017-01-01' and '2017-01-31'
 group by a.rednibroj
 order by 2 desc;
 
---DODAJ DOBAVLJACE NA TO
+                                                         --DODAJ DOBAVLJACE NA VEC NAPRAVLJENU TABLICU
 
 
 select a.rednibroj, d.naziv,
@@ -322,7 +337,7 @@ group by a.rednibroj, d.naziv
 order by 3 desc;
 
 
---KADA PODATAK NIJE ZAPISAN U TABLICI A HOCES GA FILTRIRATI---having sum
+                                           --KADA PODATAK NIJE ZAPISAN U TABLICI A HOCES GA FILTRIRATI---having sum
 
 select a.rednibroj, d.naziv,
 sum (b.kolicina * b.cijena) as ukupniiznos
@@ -335,7 +350,7 @@ group by a.rednibroj, d.naziv
 having sum (b.kolicina * b.cijena) > 7000000;
 
 
---AVERAGE, MINIMAL, MAXIMAL
+                                                           --AVERAGE, MINIMAL, MAXIMAL
 
 
 select a.rednibroj, d.naziv,
@@ -352,14 +367,14 @@ group by a.rednibroj, d.naziv
 having sum (b.kolicina * b.cijena) > 7000000;
 
 
---SELECT COUNT
+                                                                 --SELECT COUNT
 
 select count(*) from artikli;
 
 select distinct artikl from artiklinaprimci;
 
 
---OBRISATI I NACI NEKORISTENE ARTIKLE
+                                                   --OBRISATI I NACI NEKORISTENE ARTIKLE
 
 select * from artikli where sifra
 not in (select distinct artikl from artiklinaprimci);
@@ -367,4 +382,184 @@ not in (select distinct artikl from artiklinaprimci);
 
 delete from artikli where sifra
 not in (select distinct artikl from artiklinaprimci);
+
+  
+select count(*) from artikli
+where cijena between 1000 and 1100;
+
+-- Izlistati sva mjesta u općini Čepin
+select b.*
+from opcine a inner join mjesta b
+on a.sifra = b.opcina
+where a.naziv='Čepin';
+
+
+
+
+
+
+
+-- postavite Beketincima poštanski broj 31431
+update mjesta set postanskiBroj='31431' where sifra=1945;
+
+-- Izlistajte imena i prezimena svih kupaca iz mjesta Livana
+select b.ime, b.prezime
+from mjesta a inner join kupci b
+on b.mjesto = a.sifra
+where a.naziv='Osijek';
+
+-- koliko je bilo primki u 2016
+select top 1 * from primke where 
+datum between '2017-01-01' and '2017-12-31';
+
+-- Što smo sve zaprimili na primci 14778/2017
+select c.kratkinaziv, b.kolicina, b.cijena,
+b.kolicina * b.cijena as iznos
+from primke a 
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl = c.sifra
+where a.rednibroj='14778/2017'
+order by 4 desc;
+
+
+-- ovo je istovjetno gornjem upitu
+select artikli.kratkinaziv, artiklinaprimci.kolicina, artiklinaprimci.cijena,
+artiklinaprimci.kolicina * artiklinaprimci.cijena as iznos
+from primke 
+inner join artiklinaprimci on primke.sifra=artiklinaprimci.primka
+inner join artikli on artiklinaprimci.artikl = artikli.sifra
+where primke.rednibroj='14778/2017'
+order by 4 desc;
+
+
+-- zanima me ukupni iznos na toj primci
+
+select sum(b.kolicina * b.cijena) as ukupniIznos
+from primke a 
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl = c.sifra
+where a.rednibroj='14778/2017';
+
+-- koliki su iznosi na svim primkama u sijećnju 2017. godine
+select a.rednibroj, sum(b.kolicina * b.cijena) as ukupniIznos
+from primke a 
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl = c.sifra
+where a.datum between '2017-01-01' and '2017-01-31'
+group by a.rednibroj
+order by 2 desc;
+
+
+-- dovucite naziv dobavljača u upit
+
+select a.rednibroj, d.naziv,
+sum(b.kolicina * b.cijena) as ukupniIznos,
+avg(b.kolicina * b.cijena) as prosjek,
+min(b.kolicina * b.cijena) as minimalno,
+max(b.kolicina * b.cijena) as maksimalno
+from primke a 
+inner join artiklinaprimci b on a.sifra=b.primka
+inner join artikli c on b.artikl = c.sifra
+inner join dobavljaci d on a.dobavljac=d.sifra
+where a.datum between '2017-01-01' and '2017-12-31'
+group by a.rednibroj, d.naziv
+having sum(b.kolicina * b.cijena)>7000000
+order by 3 desc;
+
+-- traženje igle u plastu sijena
+-- Obrisa<ti sve artikle koji nisu nikada nabavljeni
+select count(*) from artikli;
+select distinct artikl from artiklinaprimci;
+-- podupit
+select * from artikli where sifra
+not in (select distinct artikl from artiklinaprimci);
+
+--  Obrišite ta dva artikla
+delete from artikli where sifra
+not in (select distinct artikl from artiklinaprimci);
+
+-- * sve kolone
+select * from smjerovi;
+
+-- nazivi kolona
+select naziv, cijena from smjerovi;
+
+-- konstanta
+select naziv, 'Osijek', 8, 9.8 from smjerovi;
+
+-- kolonama se mo�e dati zamjensko ime
+select naziv as smjer, 'Osijek' as grad from smjerovi;
+
+-- izraz
+select naziv, len(naziv) as duzina, 
+getdate() as datum from smjerovi;
+
+-- najkompliciranji primjer
+select sifra as s, naziv as n, 'Edunova' as skola,
+len(naziv) as duzina, * from smjerovi;
+
+-- filtriranje redova - where dio
+
+select * from smjerovi where sifra=1;
+
+-- operatori
+-- uspore�ivanje = > >= < <= !=
+select * from polaznici where sifra!=7;
+
+select * from polaznici where ime='Ivan';
+
+select * from grupe where datumpocetka>'2023-09-01';
+
+-- logi�ke operatori: and, or
+select * from polaznici where
+not (ime='Ivan' or ime='Ivor');
+
+-- ostali operatori: like
+select * from polaznici where ime not like '%a%';
+
+-- koji su polaznici s po�etnim slovom prezimena B
+select * from polaznici where prezime like 'B%�i�';
+
+-- kaladont
+select * from polaznici where prezime like '%nt';
+
+select * from polaznici where 
+sifra=2 or sifra=4 or sifra=7;
+
+-- jednostavnija sintaksa
+select * from polaznici where sifra in (2,4,7);
+
+select * from polaznici where
+sifra>=6 and sifra<=10;
+
+-- jednostavnija sintaksa
+select * from polaznici where
+ sifra between 6 and 10;
+
+select * from grupe where
+datumpocetka between '2023-01-01' and '2023-12-31';
+
+-- na null vrijednosti u tavlicama ne mo�e se primjenjivati niti jedan prethodno opisan operator
+select * from smjerovi where brojsati=NULL;
+
+-- koristi se: is null, is not null
+select * from smjerovi where brojsati is not null;
+
+select isnull(brojsati,30) as bs from smjerovi;
+
+
+-- slaganje podataka
+
+select * from polaznici order by prezime desc, 2 desc;
+
+-- limitiranje podataka
+
+select top 10 * from polaznici;
+
+select top 10 percent * from polaznici;
+
+
+select ime as djever, brojugovora as racun 
+into nova
+from polaznici; 
 
